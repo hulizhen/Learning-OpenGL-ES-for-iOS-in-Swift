466d9c0ebadd4bbc622f33ef6fdae4f5089311af
Lizhen Hu <ihulizhen@gmail.com>
2018-03-06 08:51:50 +0800

    Create OpenGLES-Ch3-1

---
+++ OpenGLES-Ch3-1/OpenGLES-Ch3-1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenGLES-Ch3-1
+//
+//  Created by Lizhen Hu on 06/03/2018.
+//  Copyright © 2018 Lizhen Hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
